201b0358dc2f0f81a576ca83a8c512e769a2a4b5
pedro-santos <pedroabs.dev42@gmail.com>
2020-07-05 23:47:35 -0300

    felipe-01

--- WebAppPedidos/Startup.cs
+++ WebAppPedidos/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WebAppPedidos.Data;
-using WebAppPedidos.Models;
 
 namespace WebAppPedidos
 {
